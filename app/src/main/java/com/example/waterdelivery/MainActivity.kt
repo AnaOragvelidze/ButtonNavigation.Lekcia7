315ad01da609133979499506742e606bdb4f7372
Anka Oragvelidze <ankaoragvelidze@Ankas-MacBook-Pro.local>
2023-04-19 14:19:16 +0400

    Initial commit

---
+++ app/src/main/java/com/example/waterdelivery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.waterdelivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+
+    }
